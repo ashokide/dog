f42c5d24e0a9e2ff2611e432090803218ae4d938
Ashok N <66304695+ashokide@users.noreply.github.com>
2022-11-08 17:45:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const url = document.getElementById('url')
-const name = document.getElementById('name')
-const bred_for = document.getElementById('bred_for')
-const breed_group = document.getElementById('breed_group')
-const life_span = document.getElementById('life_span')
-const temperament = document.getElementById('temperament')
+// const name = document.getElementById('name')
+// const bred_for = document.getElementById('bred_for')
+// const breed_group = document.getElementById('breed_group')
+// const life_span = document.getElementById('life_span')
+// const temperament = document.getElementById('temperament')
 
 const getDogData = async () => {
     const raw_data = await fetch('https://api.thedogapi.com/v1/images/search')
@@ -13,17 +13,18 @@ const getDogData = async () => {
 
 const reloadData = () => {
     getDogData().then(result => {
-        if (result[0].breeds[0] === undefined) {
-            reloadData()
-        }
-        else {
-            url.src = result[0].url
+           url.src = result[0].url
+//         if (result[0].breeds[0] === undefined) {
+//             reloadData()
+//         }
+//         else {
+//             url.src = result[0].url
 //             name.innerText = result[0].breeds[0].name
 //             bred_for.innerText = result[0].breeds[0].bred_for
 //             breed_group.innerText = result[0].breeds[0].breed_group
 //             life_span.innerText = result[0].breeds[0].life_span
 //             temperament.innerText = result[0].breeds[0].temperament
-        }
+//         }
     })
 }
 
